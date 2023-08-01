4cbc851fcdb271d81372c0dbf47514a642e7819e
renawmontanari <regiangelica@gmail.com>
2023-08-01 09:08:10 -0300

    ReqHTTP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 
 import './App.css';
 
+import {useState, useEffect} from "react";
+
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // Resgatando dados
+  useEffect(() => { 
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-     <h2>Requisição HTTP</h2>
+     <h2>Lista de Produtos</h2>
+     <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} - Cor: {product.color} - R$: {product.price}
+        </li>
+      ))}
+     </ul>
     </div> 
   );    
 }
