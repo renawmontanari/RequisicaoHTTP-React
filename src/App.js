9e6e49f52a91f8c41283bdc45837b8c302e3d287
renawmontanari <regiangelica@gmail.com>
2023-08-02 23:59:21 -0300

    ReqHTTP

--- src/App.js
+++ src/App.js
@@ -104,7 +104,9 @@ function App() {
               onChange={(e) => setPrice(e.target.value)}
             />
           </label>
-          <input type="submit" value="Criar" />
+          {/* State de loading no post */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Criar" />}
         </form>
      </div>
     </div> 
